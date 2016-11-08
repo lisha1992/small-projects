f87955f7a500527fe82a649068e601729f602818
lisha1992 <cecilia.ly@outlook.com>
2016-11-08 23:03:31 +0800

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,17 +5,15 @@ Created on Tue Jun 28 19:29:55 2016
 @author: ceciliaLee
 """
 ########################  Load data  ######################## 
-## 使用 pandas.read_csv加载数据，这样我们就有了可用于探索数据的DataFrame。
 import pandas as pd
 import statsmodels.api as sm
 import pylab as pl
 import numpy as np
  
-# 加载数据
-# 备用地址: http://cdn.powerxing.com/files/lr-binary.csv
+
 df = pd.read_csv("http://www.ats.ucla.edu/stat/data/binary.csv")
  
-# 浏览数据集
+# Scan dataset
 print df.head()
 #    admit  gre   gpa  rank
 # 0      0  380  3.61     3
@@ -23,8 +21,8 @@ print df.head()
 # 2      1  800  4.00     1
 # 3      1  640  3.19     4
 # 4      0  520  2.93     4
-# 数据集中的前三列可作为预测变量(predictor variables)
-# 第四列admit则是二分类目标变量(binary target variable)，它表明考生最终是否被录用。
+# [:,:3] are predictor variables
+# [:,4] admit is binary target variable，it represents whether the syudent is enrolled or not
  
 # 重命名'rank'列，因为dataframe中有个方法名也为'rank'
 df.columns = ["admit", "gre", "gpa", "prestige"]
